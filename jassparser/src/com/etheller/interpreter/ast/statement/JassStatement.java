60a5eebf854343272508692fc9ca4198ed940775
thatkyle <thatkyle@gmail.com>
2023-12-12 04:42:03 -0600

    Initial commit

--- jassparser/src/com/etheller/interpreter/ast/statement/JassStatement.java
+++ jassparser/src/com/etheller/interpreter/ast/statement/JassStatement.java
@@ -5,8 +5,26 @@ import com.etheller.interpreter.ast.scope.LocalScope;
 import com.etheller.interpreter.ast.scope.TriggerExecutionScope;
 import com.etheller.interpreter.ast.value.JassValue;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public interface JassStatement {
 	// When a value is returned, this indicates a RETURN statement,
 	// and will end outer execution
 	JassValue execute(GlobalScope globalScope, LocalScope localScope, TriggerExecutionScope triggerScope);
+	
+	default boolean isSleepStatement(JassStatement statement) {
+		
+	};
+
+	default JassValue delayedExecute(JassStatement statement, GlobalScope globalScope, 
+			LocalScope localScope, TriggerExecutionScope triggerScope, long delay) {
+		ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+		executor.schedule(() -> {
+		    	statement.execute(globalScope, localScope, triggerScope);
+		}, delay, TimeUnit.MILLISECONDS);
+		executor.shutdown();
+		return null;
+	}
 }
